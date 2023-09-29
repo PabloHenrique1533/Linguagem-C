ba97e185dbf8e4ed39287a8b5b1c51335bbd324c
pablo.silva288@etec.gov.sp.br <pablo.silva288@etec.gov.sp.br>
2023-09-29 16:51:24 -0300

    tudo crt

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {	
+	printf("hello\n");
+	printf("nao quero prova\n");
+	printf("jefinho vai fuder com nois\n");
+	printf("sem cuspe ainda");
+	
+return 0;
+}
