9284122169716f3770ee08091a9d8220935aa410
Pablo Henrique <pablo.silva288@etec.sp.gov.br>
2023-10-21 21:12:05 -0300

    Adicionando as funçoes e arrumando pasta

---
+++ funçoes/blibioteca.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float soma(float num1, float num2){
+	int resultado;
+	resultado= num1+num2;
+	return resultado;
+}
+
+float subtracao(float num1, float num2){
+	int resultado;
+	resultado = num1-num2;
+	return resultado;
+}
+
+float multiplicacao(float num1, float num2){
+	int resultado;
+	resultado = num1*num2;
+	return resultado;
+}
+
+float divisao(float num1, float num2){
+	int resultado;
+	resultado = num1/num2;
+	return resultado;
+}
